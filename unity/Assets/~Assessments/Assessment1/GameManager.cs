581e84d54abe284173fbfa0863b71ac235ee13a5
X7Jimmy <james2001leong@gmail.com>
2018-06-19 13:59:47 +1000

    Assessment

---
+++ unity/Assets/~Assessments/Assessment1/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private float size;
+
+    [SerializeField]
+    GameObject platform;
+    Vector3 lastpos;
+	void Start () {
+        size = platform.transform.localScale.x;
+        lastpos = platform.transform.position;
+        for (int x = 0; x < 24; x++);
+        {
+            SpawnZ();
+        }
+        InvokeRepeating("SpawnPlatform", 2f, 0.2f);
+	}
+	
+	void Update () {
+		
+	}
+    private void SpawnPlatform()
+    {
+        int random = Random.Range(0, 6);
+        if (random < 3)
+            SpawnX();
+
+        if (random >= 3)
+            SpawnZ();
+    }
+    private void SpawnX()
+    {
+        GameObject _platform = Instantiate(platform) as GameObject;
+        _platform.transform.position += new Vector3(size, 0f, 0f);
+        lastpos = platform.transform.position;
+    }
+
+    private void SpawnZ()
+    {
+        GameObject _platform = Instantiate(platform) as GameObject;
+        _platform.transform.position += new Vector3(0f, 0f, size);
+        lastpos = platform.transform.position;
+
+    }
+}
---
+++ unity/Assets/~Assessments/Assessment1/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private bool ismovingright = false;
+    private bool canmove = true;
+
+
+    [SerializeField]
+    float speed = 4f;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canmove)
+        {
+            ChangeBoolean();
+            ChangeDirection();
+        }
+        if (Physics.Raycast(this.transform.position, Vector3.down * 2) == false)
+        {
+            FallDown();
+        }
+    }
+
+    private void FallDown()
+    {
+        canmove = false;
+        rb.velocity = new Vector3(0f, -4f, 0f);
+    }
+
+    private void ChangeBoolean()
+    {
+        ismovingright = !ismovingright;
+    }
+    private void ChangeDirection()
+    {
+        if (ismovingright)
+        {
+            rb.velocity = new Vector3(speed, 0f, 0f);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0f, 0f, speed);
+        }
+    }
+}
